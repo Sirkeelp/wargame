698467860edc86cd6bbd09dba0b0f9761c00bed3
Ezequiel Alarcon <ps3ezequiel@gmail.com>
2024-05-26 16:31:36 +0200

    board 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const board_col = ['a','b', 'c', 'd', 'e',' f']
+const board_row = ['1','2', '3', '4', '5','6']
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Button({children}) {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button className="h-10 w-10 p-1 border-2 hover:bg-gray-200">
+          {children}
+      </button>
+    </>
+  )
+}
+
+function App() {
+  return (
+    <>
+      <section className="p-3">
+        {
+          board_col.map((letter, index) => {
+            return (
+              <>
+                {
+                  board_row.map((num) =>
+                  <Button key={index + letter + num}>
+                    <p>{letter.toUpperCase() + num}</p>
+                  </Button>
+                  )
+                }
+                <br />
+              </>
+              )
+          })
+        }
+      </section>
     </>
   )
 }
